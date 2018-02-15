8acbfc50a69c536cf6f52bdb5ee80737c100589d
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-02-15 09:12:07 -0500

    fixed lives-left counter

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-/*
-PSEUDOCODE
-
-1. user 1 receives a prompt to set the string. string not allowed to contain numbers
-2. after user 1 enters string, each character value of that string are passed into an array; hidden answer array
-3. based on the length of the hidden ans array, make a space with an underscore for each index.
-4. there is a set of 26 divs for each letter in the alphabet. each one has a value corresponding to the display letter
-5. When letter div is clicked, value of that div is checked against every index in underscore array using if else statement
-If 
-    Value is equivalent, push that value into the corresponding index, and add +1 to reveal counter.
-    Also, make the letter div turn a diff color and become unclickable, or disappear.
-    Finally, if reveal counter = # of spaces in the array, go to win screen.
-Else
-    Value is not equivalent. Make the letter div turn a diff color and become unclickable, or disappear.
-    Add +1 to "mistake" counter. This is a visible counter
-    If mistake counter = 5, go to loss screen
-*/
-
 /* TODO list
 
 2. add life total based on # of letters in the word
@@ -57,8 +39,8 @@ lettersGuessedDisplay.innerHTML = lettersGuessed;
 
 //make mistake counter
 var mistakeCounter = 7;
-// var livesLeftDisplay = document.querySelector('.lives-left');
-// livesLeftDisplay.innerHTML =
+var livesLeftDisplay = document.querySelector('.lives-left');
+livesLeftDisplay.innerHTML = mistakeCounter;
 
 // get parent element for the buttons (.letters)
 var letters = document.querySelector('.letters');
@@ -80,8 +62,9 @@ if (event.srcElement.nodeName === "BUTTON") {
         } else if (hiddenWordArray.includes(event.target.innerHTML) === false ) {
             i = hiddenWordArray.length; // CHECK TO MAKE SURE THIS ISN'T A PROBLEM
             mistakeCounter -= 1;
+            livesLeftDisplay.innerHTML = mistakeCounter;
             if (mistakeCounter === 0) {
-                console.log('yo ufuckin lost m8');
+                
                 }
         }
     }
