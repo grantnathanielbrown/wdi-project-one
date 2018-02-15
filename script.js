a8004217d9140ab9011f53038b659a1e43cdd654
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-02-15 09:36:06 -0500

    added very precise comments to the JS file, describing almost everything that happens in a step by step fashion

--- script.js
+++ script.js
@@ -17,50 +17,69 @@ console.log(wordSelector);
 // this turns user 1 string into values of hidden array
 var hiddenWordArray = wordSelector.split("");
 console.log(hiddenWordArray);
+// this creates an array(underscoreArray) which will later be filled with underscores.
 var underscoreArray = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",];
 console.log(underscoreArray);
+// this fills the underscoreArray with underscores based on the length of the word chosen by user 1
 underscoreArray.fill("_", 0, hiddenWordArray.length)
 console.log(underscoreArray);
 
 
-
+// display variable is created to take innerHTML from the underscoreArray
 var hangmanDisplay = document.querySelector('.underscore-array');
+// then, array is temporarily turned into a string so it can be passed into the display variable
 underscoreArray = underscoreArray.join(' ');
-console.log(underscoreArray);
+// innerhtml of display variable is set to underscoreArray (temporarily a string)
 hangmanDisplay.innerHTML = underscoreArray;
+// underscoreArray is turned back into an array, so when our user 2 clicks buttons, we can check the innerhtml values against underscoreArray
 underscoreArray = underscoreArray.split(' ');
-console.log(underscoreArray);
 
-
-var lettersGuessed = "Letters Guessed: ";   
+// creates a string which will have the values of each letter guessed pushed to the end of it
+var lettersGuessed = "Letters Guessed: ";
+// creates display variable for letters which have been guessed 
 var lettersGuessedDisplay = document.querySelector('.letters-guessed-array');
+// set innerhtml of display variable to the value of the lettersGuessed string, which will later be added onto
 lettersGuessedDisplay.innerHTML = lettersGuessed;
 
 
-//make mistake counter
-var mistakeCounter = 7;
+// make mistake counter based on the length of the word; a longer word means more lives are granted to user 2
+if (hiddenWordArray.length <= 5) {
+    var mistakeCounter = 5;
+} else if ( hiddenWordArray.length > 5 && hiddenWordArray.length <= 9) {
+    var mistakeCounter = 7;
+} else if (hiddenWordArray.length > 9) {
+    var mistakeCounter = 9;
+}
+// creates display variable for # of lives left
 var livesLeftDisplay = document.querySelector('.lives-left');
+// set innerhtml of display variable to value of the counter
 livesLeftDisplay.innerHTML = mistakeCounter;
 
 // get parent element for the buttons (.letters)
 var letters = document.querySelector('.letters');
-//add event listener to parent element
+// adds event listener to parent element
 letters.addEventListener('click', function checkLetter() {
+// the if statement wrapped around the entire function prevents a disastrous bug from occurring if a user clicks on the whitespace in the letters box
+// basically, it ensures the event listener only works on the buttons
 if (event.srcElement.nodeName === "BUTTON") {
+    // adds letters to lettersGuessed array
     lettersGuessed += event.target.innerHTML;
+    // updates value of lettersGuessed array
     lettersGuessedDisplay.innerHTML = lettersGuessed;
+    // makes the clicked button invisible by adding a CSS class
     event.target.classList.add('invisible');
     for (var i = 0; i < hiddenWordArray.length; i++) {
-        //checks to see if innerhtml of button is equal to letters in each index of hiddenwordarray
+        // checks to see if innerhtml of button is equal to letters in each index of hiddenwordarray
         if (event.target.innerHTML === hiddenWordArray[i]) {
-        // populates underscorearray (the visible array) with the accurately guessed letter
+            // populates underscorearray (the visible array) with the accurately guessed letter
             underscoreArray[i] = event.target.innerHTML;
-            console.log(underscoreArray);
+            // accomplishes the same thing that lines 30-35 do by changing the underscoreArray into a string, updating the value, then turning it back into an array
             underscoreArray = underscoreArray.join(' ');
             hangmanDisplay.innerHTML = underscoreArray;
             underscoreArray = underscoreArray.split(' ');
+        // if the user guesses incorrectly, decrements the livesLeft counter.
         } else if (hiddenWordArray.includes(event.target.innerHTML) === false ) {
-            i = hiddenWordArray.length; // CHECK TO MAKE SURE THIS ISN'T A PROBLEM
+            i = hiddenWordArray.length;
             mistakeCounter -= 1;
             livesLeftDisplay.innerHTML = mistakeCounter;
             if (mistakeCounter === 0) {
